3cb002e15221e4046ae280a07a11e65e5d387059
Spenser Black <spenserblack01@gmail.com>
2022-11-12 19:26:06 -0500

    Migrate on post attach instead of post create

--- .devcontainer/postAttach.sh
+++ .devcontainer/postAttach.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+python manage.py migrate
 python manage.py runserver
--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,4 +2,3 @@
 pip install -r requirements.txt
 pip install -r requirements-dev.txt
 pre-commit install
-python manage.py migrate
